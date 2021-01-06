5edc7f642ff2420795ac06e418b54f207c1f7d9a
Aturals <674188135@qq.com>
2021-01-06 10:05:57 +0800

    first commit project

---
+++ myspringtest/src/main/java/com/even/sbtest/myspringtest/MyspringtestApplication.java
@@ -0,0 +1,13 @@
+package com.even.sbtest.myspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class MyspringtestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringtestApplication.class, args);
+    }
+}
---
+++ myspringtest/src/test/java/com/even/sbtest/myspringtest/MyspringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.even.sbtest.myspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
